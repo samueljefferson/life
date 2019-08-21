fcbf469df373467f9a1f4eb9ab00ef276222fd28
samueljefferson <samuel.jefferson@rutgers.edu>
2019-08-21 18:37:51 -0400

    changed hardcoded if statements to a second group of nested for loops

--- life.c
+++ life.c
@@ -18,6 +18,10 @@ int main() {
 
 	int i = 0;
 	int j = 0;
+	int k = -1;
+	int l = -1;
+	int x = -1;
+	int y = -1;
 
 	// screen with random life
 	srand(time(NULL));
@@ -25,12 +29,14 @@ int main() {
 		for (j = 0; j < width-1; j++) {
 
 			int number = rand() % 10;
-			if (number < 5) {
+			if (number < 3) {
 				mvaddch(i,j, '*');
 			}
 		}
 	}
+
 	refresh();
+	sleep(1);
 
 	while (1) {
 		// count is initially the number of neighbors
@@ -42,138 +48,26 @@ int main() {
 			for (j=0; j < width; j++) {
 				count = 0;
 
-				// top row
-				if (i == 0) {
-					// top left
-					if (j == 0) {
-						if (mvinch(0,1) == '*') {
-							count++;
-						}
-						if (mvinch(1,0) == '*') {
-							count++;
-						}
-					} else if (j == width - 1) {
-						// top right
-						if (mvinch(0,width-2) == '*') {
-							count++;
-						}
-						if (mvinch(1,width-1) == '*') {
-							count++;
-						}
-					} else {
-						// top middle
-						// left
-						if (mvinch(i,j-1) == '*') {
-							count++;
-						}
-						// right
-						if (mvinch(i,j+1) == '*') {
-							count++;
-						}
-						// bottom
-						if (mvinch(i+1,j) == '*') {
-							count++;
-						}
-					}
-				} else if (i == height - 1) {
-					// bottom row
+				// check adjacent cells for life
+				for (k = -1; k < 2; k++) {
+					for (l = -1; l < 2; l++) {
+						y = i + k;
+						x = j + l;
+						// make sure the cell being checked is within bounds
+						if (y >= 0 && y < (height - 1) && x >= 0 && x < (width - 1)) {
+							// ignore current cell
+							if (k == l && l == 0) {}
+							 else {
+								 if (mvinch(y,x) == '*'){
+								 	count++;
+								 }
+							}
 
-					// bottom left
-					if (j == 0) {
-						if (mvinch(height-1,width-2) == '*') {
-							count++;
 						}
-						if (mvinch(height-2,width-1) == '*') {
-							count++;
-						}
-					} else if (j == width - 1) {
-						// bottom right
-						if (mvinch(height-1,1) == '*') {
-							count++;
-						}
-						if (mvinch(height-2,0) == '*') {
-							count++;
-						}
-					} else {
-						// bottom middle
-						// left
-						if (mvinch(i,j-1) == '*') {
-							count++;
-						}
-						// right
-						if (mvinch(i,j+1) == '*') {
-							count++;
-						}
-						// top
-						if (mvinch(i-1,j) == '*') {
-							count++;
-						}
-					}
-				} else if (j == 0) {
-					// left column
-
-					// top
-					if (mvinch(i-1,j) == '*') {
-						count++;
-					}
-					// right
-					if (mvinch(i,j+1) == '*') {
-						count++;
-					}
-					// bottom
-					if (mvinch(i+1,j) == '*') {
-						count++;
-					}
-				} else if (j == width - 1) {
-					// right column
-					// top
-					if (mvinch(i-1,j) == '*') {
-						count++;
-					}
-					// left
-					if (mvinch(i,j-1) == '*') {
-						count++;
-					}
-					// bottom
-					if (mvinch(i+1,j) == '*') {
-						count++;
-					}
-				} else {
-					// middle
-
-					// UL
-					if (mvinch(i-1,j-1) == '*'){
-						count++;
-					}
-					// U
-					if (mvinch(i-1,j) == '*'){
-						count++;
-					}
-					// UR
-					if (mvinch(i-1,j+1) == '*'){
-						count++;
-					}
-					// L
-					if (mvinch(i,j-1) == '*'){
-						count++;
-					}
-					// R
-					if (mvinch(i,j+1) == '*'){
-						count++;
-					}
-					// BL
-					if (mvinch(i+1,j-1) == '*'){
-						count++;
-					}
-					// B
-					if (mvinch(i+1,j) == '*'){
-						count++;
-					}
-					// BR
-					if (mvinch(i+1,j+1) == '*'){
-						count++;
 					}
 				}
+				buffer[i][j] = count;
+
 				// have buffer show if cell will be alive or dead
 				if (mvinch(i,j) == '*') {
 					if (count == 2 || count == 3) {
@@ -189,7 +83,6 @@ int main() {
 					}
 				}
 				buffer[i][j] = count;
-
 			}
 		}
 
@@ -204,7 +97,6 @@ int main() {
 				}
 			}
 		}
-
 		refresh();
 		sleep(1);
 	}
